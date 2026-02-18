c23c1021d9cc6817fb4cb2d8a5c11152d6fb115a
Matthew-K310 <it@matthew-kennedy.com>
2026-02-18 12:04:38 -0600

    fix: Missing semicolon

--- modules/wm.nix
+++ modules/wm.nix
@@ -10,7 +10,7 @@
   xdg.portal = {
 	enable = true;
 	extraPortals = [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
-	config.hyprland.default = [ "hyprland" "gtk" ]
+	config.hyprland.default = [ "hyprland" "gtk" ];
   };
 
   environment.systemPackages = with pkgs; [
