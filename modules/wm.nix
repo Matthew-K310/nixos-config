b488e70d0c667236bb7b805251b576ac4df3ada2
Matthew-K310 <it@matthew-kennedy.com>
2026-02-18 12:03:31 -0600

    fix: Slow startup on waybar
    Due to conflicting xdg-desktop-portal services

--- modules/wm.nix
+++ modules/wm.nix
@@ -7,6 +7,12 @@
     xwayland.enable = true;
   };
 
+  xdg.portal = {
+	enable = true;
+	extraPortals = [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
+	config.hyprland.default = [ "hyprland" "gtk" ]
+  };
+
   environment.systemPackages = with pkgs; [
     hyprlock
     hypridle
