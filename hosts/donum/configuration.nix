a2898c006c6f7724d01b533f7424a1d5d1531a4f
Matthew-K310 <it@matthew-kennedy.com>
2026-02-19 21:04:35 -0600

    feat: Add donum to hosts

---
+++ hosts/donum/configuration.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../common.nix
+    ../../modules/cli.nix
+    ../../modules/dev.nix
+    ../../modules/emacs.nix
+    ../../modules/graphics.nix
+    ../../modules/music.nix
+    ../../modules/services.nix
+    ../../modules/wm.nix
+  ];
+
+  networking.hostName = "donum";
+
+  # Use systemd-boot instead of GRUB
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  
+  # Use latest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  users.users.matthewkennedy = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  system.stateVersion = "24.11";
+}
