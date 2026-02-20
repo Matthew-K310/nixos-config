8ba1688ad1333afe763a800f30d7d11c3000b573
Matthew-K310 <it@matthew-kennedy.com>
2026-02-19 21:08:49 -0600

    fix: Add donum to flake.nix

--- flake.nix
+++ flake.nix
@@ -47,6 +47,22 @@
             }
           ];
         };
+
+        donum = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit username; };
+          modules = [
+            ./hosts/donum/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+	      home-manager.backupFileExtension = "backup";
+              home-manager.extraSpecialArgs = { inherit username; }; 
+              home-manager.users.${username} = import ./home-manager; 
+            }
+          ];
+        };
       };
     };
 }
