74f8aec2a7fdf4bfe158ab008c48156f647f6552
Matthew-K310 <it@matthew-kennedy.com>
2026-03-05 21:50:11 -0600

    fix: duplicate xdg-portal config

--- modules/wm.nix
+++ modules/wm.nix
@@ -11,7 +11,6 @@
 	  enable = true;
 	  extraPortals = [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
 	  config.hyprland.default = [ "hyprland" "gtk" ];
-	  config.hyprland.default = [ "hyprland" "gtk" ];
   };
 
   environment.systemPackages = with pkgs; [
