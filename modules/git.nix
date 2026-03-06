b3e004ea0e7f1c9390c9f7f41e9ade7f85002158
Matthew-K310 <it@matthew-kennedy.com>
2026-03-05 23:07:41 -0600

    fix: Update git config

--- modules/git.nix
+++ modules/git.nix
@@ -1,64 +1,51 @@
 { config, lib, pkgs, ... }:
-
 {
   environment.systemPackages = with pkgs; [
-	  gh
-	  forgejo-cli
+    gh
+    forgejo-cli
     delta
   ];
-
   programs.git = {
     enable = true;
-
-    userName  = "Matthew-K310";
-    userEmail = "it@matthew-kennedy.com";
-
-    signing = {
-      key       = "E18A4E6A498526625185E1DC598BDE7E73DEB741";
-      signByDefault = true;
-    };
-
     config = {
-      core = {
-        fileMode     = true;
-        ignoreCase   = false;
-        symlinks     = true;
-        compression  = 9;
-        excludesFile = "~/.config/git/ignore";
-        attributesFile = "~/.config/git/attributes";
-        hooksPath    = "~/.config/git/hooks";
-        pager        = "delta";
-        editor       = "nvim";
+      user = {
+        name  = "Matthew-K310";
+        email = "it@matthew-kennedy.com";
+        signingKey = "E18A4E6A498526625185E1DC598BDE7E73DEB741";
       };
-
+      core = {
+        fileMode       = true;
+        ignoreCase     = false;
+        symlinks       = true;
+        compression    = 9;
+        excludesFile   = "~/.config/git/ignore";
+        attributesFile = "~/.config/git/attributes";
+        hooksPath      = "~/.config/git/hooks";
+        pager          = "delta";
+        editor         = "nvim";
+      };
       format.signOff = true;
-
       gpg = {
         program = "gpg";
         format  = "openpgp";
       };
-
       init.defaultBranch = "master";
-
-      commit.verbose = true;
-      # gpgSign is handled by signing.signByDefault above
-
+      commit = {
+        verbose  = true;
+        gpgSign  = true;
+      };
       fetch = {
         prune    = true;
         parallel = 3;
       };
-
       submodule.fetchJobs = 3;
-
       pull.rebase = false;
-
       push = {
-        gpgSign        = "if-asked";
-        default        = "simple";
+        gpgSign         = "if-asked";
+        default         = "simple";
         autoSetupRemote = true;
-        followTags     = true;
+        followTags      = true;
       };
-
       help.autoCorrect = "prompt";
     };
   };
