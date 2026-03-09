c5779784b5316db28d921a7501d028a59044f988
Matthew-K310 <it@matthew-kennedy.com>
2026-03-09 11:14:17 -0500

    fix: Remove private configs

--- dotfiles/doom/custom.el
+++ dotfiles/doom/custom.el
@@ -4,7 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ement pass password-store-menu pipewire request)))
+ '(package-selected-packages '(0x0 ement pass password-store-menu pipewire request)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
