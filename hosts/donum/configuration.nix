fb4b7125a54c7d42664d95e48b324398d31b741e
Matthew-K310 <it@matthew-kennedy.com>
2026-03-13 20:46:30 -0500

    feat: Add vintagestory to donum

--- hosts/donum/configuration.nix
+++ hosts/donum/configuration.nix
@@ -1,42 +1,46 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-    ../../common.nix
-    ../../modules/cli.nix
-    ../../modules/comms.nix
-    ../../modules/dev.nix
-    ../../modules/git.nix
-    ../../modules/emacs.nix
-    ../../modules/media.nix
-    ../../modules/network.nix
-    # ../../modules/oldskool.nix
-    ../../modules/services.nix
-    ../../modules/tools.nix
-    ../../modules/wm.nix
-  ];
+	imports = [
+		./hardware-configuration.nix
+			../../common.nix
+			../../modules/cli.nix
+			../../modules/comms.nix
+			../../modules/dev.nix
+			../../modules/git.nix
+			../../modules/emacs.nix
+			../../modules/media.nix
+			../../modules/network.nix
+# ../../modules/oldskool.nix
+			../../modules/services.nix
+			../../modules/tools.nix
+			../../modules/wm.nix
+	];
 
-  networking.hostName = "donum";
+	networking.hostName = "donum";
 
-  # Use systemd-boot instead of GRUB
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  
-  # Use latest kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+# Use systemd-boot instead of GRUB
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  # Keyboard layout
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "colemak_dh";
-  };
-  console.keyMap = "colemak";
+# Use latest kernel
+	boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  users.users.matthewkennedy = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
-  };
+# Keyboard layout
+	services.xserver = {
+		xkb.layout = "us";
+		xkb.variant = "colemak_dh";
+	};
+	console.keyMap = "colemak";
 
-  system.stateVersion = "24.11";
+	users.users.matthewkennedy = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" "networkmanager" ];
+	};
+
+	nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+		"vintagestory"
+	];
+
+	system.stateVersion = "24.11";
 }
