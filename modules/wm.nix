5b7868d51a5befe22a25a4bbc7026c0ba509ac21
Matthew-K310 <it@matthew-kennedy.com>
2026-03-19 22:25:33 -0500

    dev: Add hyprland back

--- modules/wm.nix
+++ modules/wm.nix
@@ -2,64 +2,67 @@
 { config, pkgs, ... }:
 
 {
-  # wayland
-  # programs.hyprland = {
-  #   enable = true;
-  #   xwayland.enable = true;
-  # };
+#  wayland
+	programs.hyprland = {
+		enable = true;
+		xwayland.enable = true;
+	};
 
-  # xdg.portal = {
-	#   enable = true;
-	#   extraPortals = [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
-	#   config.hyprland.default = [ "hyprland" "gtk" ];
-  # };
+	xdg.portal = {
+		enable = true;
+		extraPortals = [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
+		config.hyprland.default = [ "hyprland" "gtk" ];
+	};
 
-  # environment.systemPackages = with pkgs; [
-  #   # niri
-  #   hyprlock
-  #   hypridle
-  #   hyprnotify
-  #   hyprutils
-  #   hyprcursor
-  #   swww
-  #   waybar
-  #   swaybg
-  #   swaynotificationcenter
-  #   swayimg
-  #   wmenu
-  #   wofi
-  #   grim
-  #   slurp
-  #   nordzy-icon-theme
-  #   nordzy-cursor-theme
-  #   wl-clipboard
-	#   wl-kbptr
-	#   wlrctl
-  # ];
+	environment.systemPackages = with pkgs; [
+# niri
+		hyprlock
+			hypridle
+			hyprnotify
+			hyprutils
+			hyprcursor
+			swww
+			waybar
+			swaybg
+			swaynotificationcenter
+			swayimg
+			wmenu
+			wofi
+			grim
+			slurp
+			nordzy-icon-theme
+			nordzy-cursor-theme
+			wl-clipboard
+			wl-kbptr
+			wlrctl
+			];
 
-  # x11
- services.xserver.enable = true;
-  services.xserver.windowManager.oxwm.enable = true;
+# x11
+	services.xserver.enable = true;
+	services.xserver.windowManager.oxwm.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    xclip
-    xrandr
-    xset
-    dunst
-    picom
-    dmenu
-    nsxiv
-    scrot
-    xwallpaper
-    slock
-	xinit
-  ];
+	environment.systemPackages = with pkgs; [
+		xclip
+			xrandr
+			xset
+			dunst
+			picom
+			dmenu
+			nsxiv
+			scrot
+			xwallpaper
+			slock
+			xinit
+			wpctl
+			pactl
+			playerctl
+	];
 
-  # for managing the brightness of displayport monitors
-  services.udev.extraRules = ''
-    SUBSYSTEM=="i2c-dev", TAG+="uaccess"
-  '';
+# for managing the brightness of displayport monitors
+	services.udev.extraRules = ''
+		SUBSYSTEM=="i2c-dev", TAG+="uaccess"
+		'';
 
-  hardware.i2c.enable = true;
+	hardware.i2c.enable = true;
 
 }
