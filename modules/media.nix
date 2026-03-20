0cc231777c82e717651ee798c125a6420ee0a81d
Matthew-K310 <it@matthew-kennedy.com>
2026-03-20 13:05:38 -0500

    please

--- modules/media.nix
+++ modules/media.nix
@@ -107,16 +107,15 @@
 			yabridgectl
 			];
 
-security.pam.loginLimits = [
-      { domain = "@audio"; item = "memlock"; type = "-"   ; value = "unlimited"; }
-      { domain = "@audio"; item = "rtprio" ; type = "-"   ; value = "99"       ; }
-      { domain = "@audio"; item = "nofile" ; type = "soft"; value = "99999"    ; }
-      { domain = "@audio"; item = "nofile" ; type = "hard"; value = "99999"    ; }
-    ];
+	security.pam.loginLimits = [
+	{ domain = "@audio"; item = "memlock"; type = "-"   ; value = "unlimited"; }
+	{ domain = "@audio"; item = "rtprio" ; type = "-"   ; value = "99"       ; }
+	{ domain = "@audio"; item = "nofile" ; type = "soft"; value = "99999"    ; }
+	{ domain = "@audio"; item = "nofile" ; type = "hard"; value = "99999"    ; }
+	];
 
-    services.udev.extraRules = ''
-      KERNEL=="rtc0", GROUP="audio"
-      KERNEL=="hpet", GROUP="audio"
-    '';
-  };
+	services.udev.extraRules = ''
+		KERNEL=="rtc0", GROUP="audio"
+		KERNEL=="hpet", GROUP="audio"
+		'';
 }
